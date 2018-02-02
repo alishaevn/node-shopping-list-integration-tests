232dc29a35e2b634627351858992485a3e721dfa
mattNat <mattsanpedro76@gmail.com>
2018-02-02 12:30:32 -0800

    all pass

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -25,49 +25,49 @@ describe('Recipes', function () {
    GET
    *****************************/
 
-  // it('should return recipes on GET', function () {
+  it('should return recipes on GET', function () {
 
-  //   return chai.request(app)
-  //     .get('/recipes')
-  //     .then(function (res) {
-  //       expect(res).to.have.status(200);
-  //       expect(res).to.be.json;
-  //       expect(res.body).to.be.a('array');
-  //       expect(res.body.length).to.be.at.least(1);
+    return chai.request(app)
+      .get('/recipes')
+      .then(function (res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.at.least(1);
 
-  //       const expectedKeys = ['id', 'name', 'ingredients'];
-  //       // loop over each object(recipe) in the response.body
-  //       res.body.forEach(function (recipe) {
-  //         expect(recipe).to.be.a('object');
-  //         expect(recipe).to.include.keys(expectedKeys);
-  //       });
-  //     });
-
+        const expectedKeys = ['id', 'name', 'ingredients'];
+        // loop over each object(recipe) in the response.body
+        res.body.forEach(function (recipe) {
+          expect(recipe).to.be.a('object');
+          expect(recipe).to.include.keys(expectedKeys);
+        });
+      });
+    });
 
 
   /*****************************
    POST
    *****************************/
 
-  // it('should add a recipe on POST', function () {
-  //   const newRecipe = {
-  //     name: 'pizza',
-  //     ingredients: ['dough', 'sauce']
-  //   };
-  //   return chai.request(app)
-  //     .post('/recipes')
-  //     .send(newRecipe)
-  //     .then(function (res) {
-  //       expect(res).to.have.status(201);
-  //       expect(res).to.be.json;
-  //       expect(res.body).to.be.a('object');
-  //       expect(res.body).to.include.keys('id', 'name', 'ingredients');
-  //       expect(res.body.id).to.not.equal(null);
-  //       expect(res.body).to.deep.equal(Object.assign(newRecipe, {
-  //         id: res.body.id
-  //       }));
-  //     });
-
+  it('should add a recipe on POST', function () {
+    const newRecipe = {
+      name: 'pizza',
+      ingredients: ['dough', 'sauce']
+    };
+    return chai.request(app)
+      .post('/recipes')
+      .send(newRecipe)
+      .then(function (res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('id', 'name', 'ingredients');
+        expect(res.body.id).to.not.equal(null);
+        expect(res.body).to.deep.equal(Object.assign(newRecipe, {
+          id: res.body.id
+        }));
+      });
+    });
 
 
   /*****************************
@@ -103,23 +103,16 @@ describe('Recipes', function () {
    DELETE
    *****************************/
 
-  // it('should return recipes on GET', function(){
+  it('should delete a recipe', function(){
 
-  //   return chai.request(app)
-  //     .get('/recipes')
-  //     .then(function(res) {
-  //       expect(res).to.have.status(200);
-  //       expect(res).to.be.json;
-  //       expect(res.body).to.be.a('array');
-  //       expect(res.body.length).to.be.at.least(1);
-
-  //       const expectedKeys = ['name', 'id', 'ingredients'];
-  //       // loop over each object(recipe) in the response.body
-  //       res.body.forEach(function(recipe) {
-  //         expect(recipe).to.be.a('object');
-  //         expect(recipe).to.include.keys(expectedKeys);
-  //       });
-  //     });
-
-});
-// });
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        expect(res).to.have.status(204);
+      });
+    });
+  });
